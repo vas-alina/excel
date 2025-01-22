961f5afb051d012a111d03c3514f73583d4e0c48
vas-alina <vasalina93@gmail.com>
2025-01-22 17:50:07 +0300

    creation of an independent structure

---
+++ src/core/DomListener.js
@@ -0,0 +1,14 @@
+export class DomListener {
+    constructor($root, listeners = []) {
+        if (!$root) {
+            throw new Error(`No $root provider for DomListener`)
+        }
+        this.$root = $root
+        this.listeners = listeners
+    }
+
+    initDOMListeners() {
+
+    }
+    removeDOMListeners() {}
+}
---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,11 @@
+import { DomListener } from "./DomListener";
+
+export class ExcelComponent extends DomListener {
+    constructor($root, options) {
+        super($root, options.listeners)
+    }
+//Возвращает шаблон компонента
+    toHTML() {
+        return ''
+    };
+};
